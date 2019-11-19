ebdf6786df5335464ab3b4c293ab8e31ec6a3ba5
NITESCU Cristian <cristian.nitescu@renault.com>
2019-11-19 16:30:11 +0100

    Fixed clahe transform

--- src/tf_container/train_helpers.py
+++ src/tf_container/train_helpers.py
@@ -114,6 +114,16 @@ def remap_throttle(data, break_range=10):
             start_range = False
     
 
+def load_image_from_file(path, clahe):
+    img = np.array(load_img(path, color_mode= "grayscale"))
+    if len(img.shape) > 2 and img.shape[2] != 1:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    if clahe:
+        claheTransform = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+        img = claheTransform.apply(img)
+    return img.reshape(120,160,1)
+
+
 def generate_enhanced_dataset(root, destination, images_count, break_range=10, blur=True, crop=0, clahe=True):
     print('Generate enhanced dataset')
     do_random_shift = True
@@ -124,7 +134,7 @@ def generate_enhanced_dataset(root, destination, images_count, break_range=10, b
     # load images
     for idx in range(0, len(data)):
         dd = data[idx]
-        img = img_to_array(load_img(os.path.join(dd[3],dd[4]), grayscale=True))
+        img = img_to_array(load_image_from_file(os.path.join(dd[3],dd[4]), clahe))
         data[idx,5] = img
 
     try:
@@ -137,9 +147,6 @@ def generate_enhanced_dataset(root, destination, images_count, break_range=10, b
     while cnt < images_count:
         for idx in range(0, len(data)):
             img = data[idx, 5]
-            if clahe:
-                claheTransform = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-                img = claheTransform.apply(img).reshape(120,160,1)
 
             if do_random_shift:
                 img = random_shift(img, 0.1, 0.0, row_axis=0, col_axis=1, channel_axis=2) 
